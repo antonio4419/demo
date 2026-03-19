21598081e07c7354c8aebd76ce2b22ce2f0c2eb1
antonio <antoniobautista.desarrollo@gmail.com>
2026-03-19 23:59:41 +0000

    generate model product

--- src/main/java/com/storage/demo/demo/models/Product.java
+++ src/main/java/com/storage/demo/demo/models/Product.java
@@ -4,10 +4,91 @@
  */
 package com.storage.demo.demo.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
 /**
  *
  * @author lab
  */
+@Entity
+@Table(name="product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(unique=true,nullable=false)
+    @DateTimeFormat(pattern = "dd/MM/yyyy") 
+    
+    private Long id;
+    private String name;
+    private String description;
+    private String image;
+    private Float price;
+    private Integer stock;
+    private LocalDate date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    
+    
     
 }
